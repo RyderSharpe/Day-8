10043c45e999c9b5d8db8f2a851a473da0cc658b
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-01-08 22:03:23 -0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,45 @@
+# #TODO simple function
+# def greet():
+#     print("Hello")
+#     print("how do you do name?")
+#     print("isnt the weather nice today?")
+#
+# greet()
+#
+#
+# # TODO function that allows for input
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"how do you do {name}?")
+#
+# greet_with_name("Ryder")
+
+
+########### NOTES ################
+
+# def my_function(something):
+#     # do this with something
+#     # then do this
+#     # finally do this
+# my_function(123)
+## something = 123 # here "something" is the parameter and "123" is the argument.
+
+##################################
+
+
+# #TODO Functions with more than 1 input
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}")
+#
+# greet_with("Ryder", "Vancouver?")
+
+
+# TODO Keyword arguments
+# Here we can switch the code around
+def keyword(a, b, c):
+    print(f"a = {a}")
+    print(f"b = {b}")
+    print(f"c - {c}")
+
+keyword(c=3, b=2, a=1)
