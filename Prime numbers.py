b591fc10a37242cb005aeb53c772567a605feb64
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-01-09 16:58:02 -0800

    Prime numbers.py

--- Prime numbers.py
+++ Prime numbers.py
@@ -17,3 +17,14 @@ def prime_checker(number):
 
 n = int(input())  # Check this number
 prime_checker(number=n)
+
+
+################ HOW IT WORKS##################
+
+# You define a list called list containing numbers from 2 to 10.
+# You create a function prime_checker that takes a number as an argument.
+# Inside the function, you use a for loop to iterate over each element in the list.
+# In each iteration, you check if the given number (n) is divisible evenly by the current prime from the list using the modulus operator (%).
+# If the number is divisible, you print "It's not a prime number." and use the break statement to exit the loop.
+# If the loop completes without finding any divisor, the else block is executed, and you print "It's a prime number."
+# Finally, you take user input for a number (n) and call the prime_checker function with that input.
